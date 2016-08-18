6974543b0906f16d165304f03ae720e465c61ef0
Nagy Károly Gábriel <karasz@jpi.io>
2016-08-18 18:49:29 +0300

    go2ban: reorganized project and handled SIGINT too.

--- utils/sigtrap.go
+++ utils/sigtrap.go
@@ -1,7 +1,7 @@
 package utils
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,25 +12,25 @@ const GoSock = "/var/run/go2ban/socket"
 func TrapSignals() {
 	go func() {
 		sigchan := make(chan os.Signal, 1)
-		signal.Notify(sigchan, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGUSR1)
+		signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGUSR1)
 
 		for sig := range sigchan {
 			switch sig {
-			case syscall.SIGTERM:
-				log.Println("[INFO] SIGTERM: Terminating process")
+			case syscall.SIGTERM, syscall.SIGINT:
+				fmt.Println("[INFO] SIGTERM or SIGINT: Terminating process")
 				os.Remove(GoSock)
 				os.Exit(0)
 
 			case syscall.SIGQUIT:
-				log.Println("[INFO] SIGQUIT: Shutting down")
+				fmt.Println("[INFO] SIGQUIT: Shutting down")
 				os.Remove(GoSock)
 				os.Exit(1)
 
 			case syscall.SIGHUP:
-				log.Println("[INFO] SIGHUP: Hanging up")
+				fmt.Println("[INFO] SIGHUP: Hanging up")
 
 			case syscall.SIGUSR1:
-				log.Println("[INFO] SIGUSR1: Reloading")
+				fmt.Println("[INFO] SIGUSR1: Reloading")
 
 			}
 		}
