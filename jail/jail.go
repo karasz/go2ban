221047b788ee0674cc78194de3969e53fb6709e7
Nagy Károly Gábriel <karasz@jpi.io>
2016-08-28 18:55:13 +0300

    go2ban: started implementing the matcher.

--- jail/jail.go
+++ jail/jail.go
@@ -3,14 +3,14 @@ package jail
 type Jail struct {
 	LogReader *LogReader
 	Executor  *Executor
-	Regex     string
+	Matcher   *Matcher
 	Enabled   bool
 }
 
-func NewJail(filename string, regex string, commands map[string]execfn, enable bool) *Jail {
+func NewJail(filename string, regex []string, commands map[string]execfn, enable bool) *Jail {
 	return &Jail{
 		Logreader: NewLogReader(filename, 0),
-		Regex:     regex,
+		Matcher:   NewMatcher(regex),
 		Executor:  NewExecutor(commands),
 		Enabled:   enabled,
 	}
