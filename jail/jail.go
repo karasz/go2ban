c4deae00dc077fee22d49ae108da021657dca9be
Nagy Károly Gábriel <karasz@jpi.io>
2016-08-29 09:43:23 +0300

    go2ban: started simplifying the jail

--- jail/jail.go
+++ jail/jail.go
@@ -2,16 +2,12 @@ package jail
 
 type Jail struct {
 	LogReader *LogReader
-	Executor  *Executor
-	Matcher   *Matcher
 	Enabled   bool
 }
 
-func NewJail(filename string, regex []string, commands map[string]execfn, enable bool) *Jail {
+func NewJail(filename string) *Jail {
 	return &Jail{
 		Logreader: NewLogReader(filename, 0),
-		Matcher:   NewMatcher(regex),
-		Executor:  NewExecutor(commands),
 		Enabled:   enabled,
 	}
 }
