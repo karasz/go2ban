c00c576b85e8029ef57af496a92fba25b58350ce
Nagy Károly Gábriel <karasz@jpi.io>
2016-09-09 13:40:44 +0300

    go2ban: started implementing go2ban server

--- g2b/main.go
+++ g2b/main.go
@@ -1,53 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"net"
+	"flag"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
-	"github.com/karasz/go2ban/common"
-	//	"github.com/karasz/go2ban/jail"
+	"github.com/karasz/go2ban/jail"
 	"github.com/karasz/go2ban/utils"
+	"github.com/naoina/toml"
 )
+
+type config struct {
+	Jaildir string
+}
 
 func init() {
 	utils.TrapSignals()
-	os.Remove(common.GoSock)
 }
 
 func main() {
-
-	l, err := net.ListenUnix("unix", &net.UnixAddr{common.GoSock, "unix"})
+	var conffile string
+	jailfiles := make([]string, 0)
+	flag.StringVar(&conffile, "f", "/etc/go2ban/g2b.toml", "specify the config file.  defaults to /etc/go2ban/g2b.toml.")
+	flag.Parse()
+	f, err := os.Open(conffile)
 	if err != nil {
-		fmt.Println("listen error:", err)
+		panic(err)
+	}
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	var config config
+	if err := toml.Unmarshal(buf, &config); err != nil {
+		panic(err)
+	}
+	f.Close()
+
+	d, err := os.Open(config.Jaildir)
+	if err != nil {
+		panic(err)
 	}
 
-	defer os.Remove(common.GoSock)
-
-	for {
-		fd, err := l.AcceptUnix()
-		if err != nil {
-			fmt.Println("accept error:", err)
-		}
-
-		go startServer(fd)
+	fls, err := d.Readdir(-1)
+	if err != nil {
+		panic(err)
 	}
 
-}
+	d.Close()
 
-func startServer(c net.Conn) {
-	for {
-		buf := make([]byte, 512)
-		n, err := c.Read(buf)
-		if err != nil {
-			return
-		}
-
-		data := buf[:n]
-		fmt.Println("Server got:", string(data))
-		_, err = c.Write(data)
-		if err != nil {
-			fmt.Println("Write: ", err)
+	for _, file := range fls {
+		if file.Mode().IsRegular() {
+			if filepath.Ext(file.Name()) == ".g2b" {
+				jailfiles = append(jailfiles, config.Jaildir+file.Name())
+			}
 		}
 	}
+
+	for _, j := range jailfiles {
+		jail := jail.NewJail(j)
+		go jail.Run()
+	}
+
+	//for now we just sleep forever
+	select {}
 }
