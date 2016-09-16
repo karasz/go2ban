d420270a4e02e6740ec09ad3d2e5a5b1d2c0b5e1
Nagy Károly Gábriel <karasz@jpi.io>
2016-09-16 19:47:59 +0300

    go2ban: removed fmt from sigtrap (not needed prints to console)

--- sigtrap.go
+++ sigtrap.go
@@ -1,7 +1,6 @@
 package go2ban
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,20 +14,12 @@ func TrapSignals() {
 		for sig := range sigchan {
 			switch sig {
 			case syscall.SIGTERM, syscall.SIGINT:
-				fmt.Println("[INFO] SIGTERM or SIGINT: Terminating process")
 				os.Exit(0)
-
 			case syscall.SIGQUIT:
-				fmt.Println("[INFO] SIGQUIT: Shutting down")
 				os.Exit(1)
-
 			case syscall.SIGHUP:
-				fmt.Println("[INFO] SIGHUP: Hanging up")
-
 			case syscall.SIGUSR1:
 				Srv.DumpCells()
-				fmt.Println("[INFO] SIGUSR1:")
-
 			}
 		}
 	}()
